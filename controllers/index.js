9d0b98be2e7023c77aaf9d6c49a5c89f9406128f
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-13 19:28:42 -0500

    changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const homeRoutes = require("./home-routes");
+const commentRoutes = require("./api/comment-routes");
+const userRoutes = require("./api/user-routes");
+const postRoutes = require("./api/post-routes");
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
+router.use("/", homeRoutes);
+router.use("/api/comments", commentRoutes);
+router.use("/api/users", userRoutes);
+router.use("/api/posts", postRoutes);
 module.exports = router;
